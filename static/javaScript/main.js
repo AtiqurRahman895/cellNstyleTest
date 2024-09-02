16156988e0bd79cf744cafb438e9a6cf4cbf83e3
AtiqurRahman895 <emonhassan895@gmail.com>
2024-09-02 12:23:42 +0600

    MADE SLIDER FUNCTIONAL AND ADDED NEXT, PREVIOUS BUTTONS

--- static/javaScript/main.js
+++ static/javaScript/main.js
@@ -11,3 +11,40 @@ window.addEventListener(`scroll`, () => {
         .classList.remove(`headerSectionAnimation`);
     }
   });
+
+
+// slider
+
+let slider=document.querySelectorAll(`.slider`)
+let firstCardWidth=document.querySelectorAll(`.firstCard`)
+let prevButton=document.querySelectorAll(`.prevButton`)
+let nextButton=document.querySelectorAll(`.nextButton`)
+
+// console.log(slider[0].scrollWidth)
+// console.log(slider[0].clientWidth)
+
+nextButton.forEach((eachNextButton,index)=>{
+  setInterval(() => {
+    if(slider[index].scrollWidth <= slider[index].clientWidth){
+      nextButton[index].style.display=`none`
+    }else{
+      nextButton[index].style.display=`inline-block`
+    }
+  }, 1000);
+
+  eachNextButton.addEventListener(`click`,(e)=>{
+    slider[index].scrollLeft +=firstCardWidth[index].offsetWidth
+  })
+})
+prevButton.forEach((eachPrevButton,index)=>{
+  setInterval(() => {
+    if(slider[index].scrollWidth <= slider[index].clientWidth){
+      prevButton[index].style.display=`none`
+    }else{
+      prevButton[index].style.display=`inline-block`
+    }
+  }, 1000);
+  eachPrevButton.addEventListener(`click`,(e)=>{
+    slider[index].scrollLeft += -firstCardWidth[index].offsetWidth
+  })
+})
